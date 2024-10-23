a44ae9ac44ce187b38d965c3ac9ce669e5c7e92d
nadir <snadir177@gmail.com>
2024-10-22 21:13:52 -0400

    update postgres adapter to handle transactions

--- internal/adapters/secondary/postgres.go
+++ internal/adapters/secondary/postgres.go
@@ -7,6 +7,7 @@ import (
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/nxdir-s/IdleRpg/internal/ports"
 )
 
 type ConnectionError struct {
@@ -18,6 +19,7 @@ func (e *ConnectionError) Error() string {
 }
 
 type PgxPool interface {
+	Begin(ctx context.Context) (pgx.Tx, error)
 	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
 	Exec(ctx context.Context, sql string, args ...interface{}) (pgconn.CommandTag, error)
 }
@@ -31,14 +33,56 @@ func NewPgxPool(ctx context.Context, dbUrl string) (*pgxpool.Pool, error) {
 	return pool, nil
 }
 
+type PgxTx interface {
+	Commit(ctx context.Context) error
+	Rollback(ctx context.Context) error
+}
+
+type PostgresOpt func(a *PostgresAdapter)
+
+func WithPgxTx(tx PgxTx) PostgresOpt {
+	return func(a *PostgresAdapter) {
+		a.tx = tx
+	}
+}
+
 type PostgresAdapter struct {
 	conn   PgxPool
+	tx     PgxTx
 	logger *slog.Logger
 }
 
-func NewPostgresAdapter(ctx context.Context, pool PgxPool, logger *slog.Logger) (*PostgresAdapter, error) {
-	return &PostgresAdapter{
+func NewPostgresAdapter(ctx context.Context, pool PgxPool, logger *slog.Logger, opts ...PostgresOpt) (*PostgresAdapter, error) {
+	adapter := &PostgresAdapter{
 		conn:   pool,
 		logger: logger,
-	}, nil
+	}
+
+	for _, opt := range opts {
+		opt(adapter)
+	}
+
+	return adapter, nil
+}
+
+func (a *PostgresAdapter) NewTransactionAdapter(ctx context.Context) (ports.DatabaseTxPort, error) {
+	tx, err := a.conn.Begin(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	txAdapter, err := NewPostgresAdapter(ctx, tx, a.logger, WithPgxTx(tx))
+	if err != nil {
+		return nil, err
+	}
+
+	return txAdapter, nil
+}
+
+func (a *PostgresAdapter) Commit(ctx context.Context) error {
+	return a.tx.Commit(ctx)
+}
+
+func (a *PostgresAdapter) Rollback(ctx context.Context) error {
+	return a.tx.Rollback(ctx)
 }
--- internal/ports/secondary.go
+++ internal/ports/secondary.go
@@ -10,3 +10,12 @@ type KafkaPort interface {
 	SendPlayerEvent(ctx context.Context, event *pb.PlayerEvent) error
 	CloseProducer() error
 }
+
+type DatabasePort interface {
+}
+
+type DatabaseTxPort interface {
+	DatabasePort
+	Commit(ctx context.Context) error
+	Rollback(ctx context.Context) error
+}
