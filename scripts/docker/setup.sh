55226f471f732041b3a77065e2a805512e6a7048
nxdir-s <snadir177@gmail.com>
2024-11-23 22:46:09 -0500

    update scripts

--- scripts/docker/setup.sh
+++ scripts/docker/setup.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
-## Usage: ./connect <connections> <number of clients> <server ip>
-
+## Usage: ./scripts/setup.sh <connections> <number of clients> <server ip>
 CONNECTIONS=$1
 REPLICAS=$2
 IP=$3
-go build --tags "static netgo" -o client client.go
+
+go build --tags "static netgo" -o ws-client cmd/ws-client/main.go
+
 for (( c=0; c<${REPLICAS}; c++ ))
 do
-    docker run -l 1m-go-websockets -v $(pwd)/client:/client -d alpine /client -conn=${CONNECTIONS} -ip=${IP}
+    docker run -l ws-client -v $(pwd)/tmp:/tmp -d alpine /ws-client -conn=${CONNECTIONS} -ip=${IP}
 done
