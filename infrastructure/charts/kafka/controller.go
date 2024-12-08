8d3ec81ddc065fdb0cb07d0a5244fb4a5dfd3451
nxdir-s <snadir177@gmail.com>
2024-12-08 01:19:43 -0500

    generate manifest

--- infrastructure/charts/kafka/controller.go
+++ infrastructure/charts/kafka/controller.go
@@ -46,19 +46,22 @@ func NewController(scope constructs.Construct, id *string, props *ControllerProp
 	label := map[string]*string{"app": cdk8s.Names_ToLabelValue(controller, nil)}
 
 	configMap := NewControllerCfg(controller, jsii.String(*id+"-cm"), &ControllerCfgProps{
-		ControllerPort: port,
-		NodeID:         props.NodeID,
+		ControllerPort: jsii.String(strconv.Itoa(int(*port))),
+		NodeID:         jsii.String(strconv.Itoa(int(*props.NodeID))),
 		ClusterID:      props.ClusterID,
 	})
 
-	k8s.NewKubeService(controller, id, &k8s.KubeServiceProps{
+	service := k8s.NewKubeService(controller, jsii.String(*id+"-srv"), &k8s.KubeServiceProps{
+		Metadata: &k8s.ObjectMeta{
+			Name: id,
+		},
 		Spec: &k8s.ServiceSpec{
 			Ports:    &[]*k8s.ServicePort{{Port: port, TargetPort: k8s.IntOrString_FromNumber(containerPort)}},
 			Selector: &label,
 		},
 	})
 
-	k8s.NewKubeDeployment(controller, id, &k8s.KubeDeploymentProps{
+	deployment := k8s.NewKubeDeployment(controller, id, &k8s.KubeDeploymentProps{
 		Spec: &k8s.DeploymentSpec{
 			Replicas: replicas,
 			Selector: &k8s.LabelSelector{
@@ -99,12 +102,14 @@ func NewController(scope constructs.Construct, id *string, props *ControllerProp
 		},
 	})
 
+	deployment.AddDependency(service)
+
 	return controller
 }
 
 type ControllerCfgProps struct {
-	ControllerPort *float64
-	NodeID         *float64
+	ControllerPort *string
+	NodeID         *string
 	ClusterID      *string
 }
 
@@ -112,12 +117,12 @@ func NewControllerCfg(scope constructs.Construct, id *string, props *ControllerC
 	return k8s.NewKubeConfigMap(scope, id, &k8s.KubeConfigMapProps{
 		Immutable: jsii.Bool(true),
 		Data: &map[string]*string{
-			"KAFKA_NODE_ID":                                  jsii.String(strconv.Itoa(int(*props.NodeID))),
+			"KAFKA_NODE_ID":                                  props.NodeID,
 			"KAFKA_PROCESS_ROLES":                            jsii.String("controller"),
-			"KAFKA_CONTROLLER_QUORUM_VOTERS":                 jsii.String(fmt.Sprintf("1@controller-1:%d,2@controller-2:%d,3@controller-3:%d", props.ControllerPort, props.ControllerPort, props.ControllerPort)),
+			"KAFKA_CONTROLLER_QUORUM_VOTERS":                 jsii.String(fmt.Sprintf("1@controller-1:%s,2@controller-2:%s,3@controller-3:%s", *props.ControllerPort, *props.ControllerPort, *props.ControllerPort)),
 			"KAFKA_INTER_BROKER_LISTENER_NAME":               jsii.String("PLAINTEXT"),
 			"KAFKA_CONTROLLER_LISTENER_NAMES":                jsii.String("CONTROLLER"),
-			"KAFKA_LISTENERS":                                jsii.String(fmt.Sprintf("CONTROLLER://:%d", props.ControllerPort)),
+			"KAFKA_LISTENERS":                                jsii.String(fmt.Sprintf("CONTROLLER://:%s", *props.ControllerPort)),
 			"CLUSTER_ID":                                     props.ClusterID,
 			"KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR":         jsii.String("3"),
 			"KAFKA_GROUP_INITIAL_REBALANCE_DELAY_MS":         jsii.String("0"),
