2584a7eec9a6a9f1f348dac766d7e4db1a2b68cc
nxdir-s <snadir177@gmail.com>
2025-01-02 04:45:47 -0500

    start auth middleware

--- internal/telemetry/spans/spans.go
+++ internal/telemetry/spans/spans.go
@@ -3,6 +3,7 @@ package spans
 const (
 	CreateUser         string = "PostgresAdapter.CreateUser"
 	RemoveUser         string = "PostgresAdapter.RemoveUser"
+	UserExists         string = "PostgresAdapter.UserExists"
 	GetUserId          string = "PostgresAdapter.GetUserID"
 	UpdateRefreshToken string = "PostgresAdapter.UpdateRefreshToken"
 	EmailExists        string = "PostgresAdapter.EmailExists"
